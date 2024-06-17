09afe46a756fa474ffba6ff81ea21352cb81af4b
lumenpearson <lumenpearson@gmail.com>
2024-06-17 21:09:06 +0300

    chore(deps): package.json & pnpm-lock.yaml new dependencies

--- src/types/interceptor-custom-response.interface.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Custom response interface for the interceptor.
- */
-export interface InterceptorCustomResponse extends Response {
-	/** Function to get headers from the response */
-	getHeaders: () => Record<string, string>;
-	/** HTTP status code of the response */
-	statusCode: number;
-}
