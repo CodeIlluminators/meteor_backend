1af81504aedc0a515f156ec2ea4dc4efe48fd4ab
lumenpearson <lumenpearson@gmail.com>
2024-06-18 15:31:37 +0300

    feat: enhance response interceptor and refactor metadata generation - Updated the response interceptor to include additional metadata such as request/response metadata. - Moved metadata generation logic to a separate utility module to adhere to the DRY principle.

--- src/common/interceptors/extended-transform.interceptor.ts
+++ src/common/interceptors/extended-transform.interceptor.ts
@@ -5,17 +5,17 @@ import {
 	NestInterceptor,
 } from "@nestjs/common";
 import { HttpArgumentsHost } from "@nestjs/common/interfaces";
+import { Request, Response } from "express";
 import * as moment from "moment-timezone";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
+import { ResponseMeta } from "../types";
 import {
-	InterceptorCustomRequest,
-	InterceptorCustomResponse,
-	InterceptorRequestMeta,
-	InterceptorResponseMeta,
-} from "../types";
-import { calculateSize } from "../utility/utility";
+	getRequestMeta,
+	getResponseMeta,
+	getServerMeta,
+} from "../utils/metadata.util";
 
 /**
  * Intercepts incoming requests and outgoing responses to transform the response format.
@@ -26,21 +26,18 @@ import { calculateSize } from "../utility/utility";
 @Injectable()
 export class ExtendedTransformInterceptor<T> implements NestInterceptor<T> {
 	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-		const httpContext: HttpArgumentsHost = context.switchToHttp();
 		const startTime: number = Date.now();
-		const request: InterceptorCustomRequest =
-			httpContext.getRequest<InterceptorCustomRequest>();
-		const response: InterceptorCustomResponse =
-			httpContext.getResponse<InterceptorCustomResponse>();
 		const timezone: string = moment.tz.guess();
+
+		const httpContext: HttpArgumentsHost = context.switchToHttp();
+		const request: Request = httpContext.getRequest<Request>();
+		const response: Response = httpContext.getResponse<Response>();
 
 		return next.handle().pipe(
 			map((data: T) => {
-				const processingTime: number = Date.now() - startTime;
-
-				const responseMeta: InterceptorResponseMeta = this.getResponseMeta(
+				const responseMeta: ResponseMeta = getResponseMeta(
 					response,
-					processingTime,
+					Date.now() - startTime,
 					data,
 				);
 
@@ -48,71 +45,12 @@ export class ExtendedTransformInterceptor<T> implements NestInterceptor<T> {
 					statusCode: response.statusCode,
 					data,
 					meta: {
-						server: this.getServerMeta(timezone),
-						request: this.getRequestMeta(request),
+						server: getServerMeta(timezone),
+						request: getRequestMeta(request, timezone),
 						response: responseMeta,
 					},
 				};
 			}),
 		);
-	}
-
-	/**
-	 * Gets server metadata including the timezone and current time.
-	 * @param timezone - The server's timezone.
-	 * @returns An object containing server metadata.
-	 */
-	private getServerMeta(timezone: string): {
-		timezone: string;
-		currentTime: string;
-	} {
-		return {
-			timezone,
-			currentTime: moment().tz(timezone).format(),
-		};
-	}
-
-	/**
-	 * Gets metadata about the request.
-	 * @param request - The incoming request object.
-	 * @returns An object containing request metadata.
-	 */
-	private getRequestMeta(
-		request: InterceptorCustomRequest,
-	): InterceptorRequestMeta {
-		const { originalUrl, method, ip: clientIp, headers, body, query } = request;
-
-		return {
-			url: originalUrl,
-			method,
-			clientIp,
-			time: moment().tz(moment.tz.guess()).format(),
-			size: `${calculateSize(body)} bytes`,
-			headers,
-			userAgent: headers["user-agent"],
-			path: request.path,
-			queryParams: query,
-			body,
-		};
-	}
-
-	/**
-	 * Gets metadata about the response.
-	 * @param response - The outgoing response object.
-	 * @param processingTime - The time taken to process the request.
-	 * @param data - The response data.
-	 * @returns An object containing response metadata.
-	 */
-	private getResponseMeta(
-		response: InterceptorCustomResponse,
-		processingTime: number,
-		data: T,
-	): InterceptorResponseMeta {
-		return {
-			time: moment().tz(moment.tz.guess()).format(),
-			processingTime: `${processingTime}ms`,
-			size: `${calculateSize(data)} bytes`,
-			headers: response.getHeaders(),
-		};
 	}
 }
